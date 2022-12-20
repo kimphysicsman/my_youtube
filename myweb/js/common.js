42327aeae7f6a91129bc9d502302a422da468e05
DongwooKim <kimphysicsman@gmail.com>
2022-12-20 19:23:30 +0900

    feat/ 웹페이지 수정

--- myweb/js/common.js
+++ myweb/js/common.js
@@ -1,6 +1,12 @@
 window.onload = () => {
   console.log("page load");
-  get_video_chart();
+  let video_chart;
+  get_video_chart().then((data) => {
+    video_chart = data;
+    console.log(video_chart);
+    change_youtube_player(video_chart["rank_1"]);
+    set_video_chart(video_chart);
+  });
 };
 
 async function get_video_chart() {
@@ -11,6 +17,21 @@ async function get_video_chart() {
     .then((response) => response.json())
     .then((data) => {
       video_chart = data;
-      console.log(video_chart);
     });
+
+  return video_chart;
+}
+
+function change_youtube_player(video_id) {
+  const player = document.getElementById("ytplayer");
+
+  player.setAttribute("src", "https://www.youtube.com/embed/" + video_id);
+}
+
+function set_video_chart(video_chart) {
+  document.getElementById("rank_1").textContent = video_chart["rank_1"];
+  document.getElementById("rank_2").textContent = video_chart["rank_2"];
+  document.getElementById("rank_3").textContent = video_chart["rank_3"];
+  document.getElementById("rank_4").textContent = video_chart["rank_4"];
+  document.getElementById("rank_5").textContent = video_chart["rank_5"];
 }
